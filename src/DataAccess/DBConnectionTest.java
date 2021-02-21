2a5ec6df930f81c38820527d877246d5a71a1b11
Adam Smith <asmith6@luc.edu>
2021-02-21 16:25:00 -0600

    DB Connection working...

--- src/DataAccess/DBConnectionTest.java
+++ src/DataAccess/DBConnectionTest.java
@@ -10,9 +10,9 @@ public class DBConnectionTest {
         "jdbc:mysql://localhost:3306/facilitymanagementsystem","root","root");  
         //here sonoo is database name, root is username and password  
         Statement stmt=con.createStatement();  
-        ResultSet rs=stmt.executeQuery("select * from facilites");  
+        ResultSet rs=stmt.executeQuery("select * from facilities");  
         while(rs.next())  
-        System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
+        System.out.println(rs.getInt(1));  
         con.close();  
         }catch(Exception e){ System.out.println(e);}  
         }  
